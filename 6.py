4e988665c911b72df1ca7abe9b58e20fd31e0302
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2022-11-01 04:41:04 +0530

    Day-6.py

---
+++ 6.py
@@ -0,0 +1,32 @@
+
+#import sys
+#sys.path.append('C:\Users\Nitish Malang')
+
+
+#from my_module import find_index , test
+
+
+#courses = ['History', 'Math', 'Physics', 'CompSci']
+
+#index = find_index(courses , 'Math')
+#print(index)
+#print(test)
+
+#print(sys.path)
+
+#import datetime
+#import calendar
+
+#courses = ['History' , 'Math' , 'Physics' , 'CompSci']
+
+#today = datetime.date.today()
+
+#print(today)
+
+#print(calendar.isleap(2020))
+
+#import os
+
+#print(os.getcwd())
+#print(os.__file__)
+import antigravity
