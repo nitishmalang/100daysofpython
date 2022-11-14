ff5a6c1e70d4a4d0905e3d5f6f6a5f7db3ee1e61
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2022-11-14 23:51:32 +0530

    Add files via upload

---
+++ formatting.py
@@ -0,0 +1,62 @@
+#txt = "For only {price:.2f} dollars!"
+#print(txt.format(price = 49))
+
+#txt1 = "My name is {fname}, I'm {age}".format(fname = "John", age = 36)
+#txt2 = "My name is {0}, I'm {1}".format("John" , 36)
+#txt3 = "My name is {} , I'm {}".format("John", 36)
+
+#txt = "We have {:<8} chickens."
+#print(txt.format(49))
+
+#txt = "We have {:>8} chickens."
+#print(txt.format(49))
+
+#txt = "We have {:^8} chickens."
+#print(txt.format(49))
+
+#txt = "The temperature is {:=8} degrees celsius."
+
+#print(txt.format(-5))
+
+#txt = "The temperature is between {:+} and {:+} degrees celsius."
+
+#print(txt.format(-3,7))
+
+#txt = "The temperature is between {: } and {: } degrees celsius."
+
+#print(txt.format(-3, 7))
+
+#txt = "The universe is {:,} years old."
+
+#print(txt.format(13800000000))
+
+#txt = "The binary version {0} is {0:b}"
+
+#print(txt.format(5))
+
+#txt = "We have {:d} chickens."
+
+#print(txt.format(0b101))
+
+#txt = "We have {:e} chickens."
+
+#print(txt.format(5))
+
+#txt = "We have {:e} chickens."
+
+#print(txt.format(5))
+
+#txt = "The octal version of {0} is {0:o}"
+
+#print(txt.format(10))
+
+#txt = "The Hexadecimal version of {0} is {0:x}"
+
+#print(txt.format(255))
+
+#txt = "We have {:E} chickens."
+#print(txt.format(5))
+
+txt = "The hexadecimal version of {0} is {0:X}"
+
+print(txt.format(255))
