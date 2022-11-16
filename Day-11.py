97a8d4e3235584d552d094ebae3553183e15cf0f
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2022-11-16 21:23:39 +0530

    Create Day-11.py

---
+++ Day-11.py
@@ -0,0 +1,57 @@
+#import random
+
+#value = random.randint(0, 1)
+#print(value)
+
+#import random
+
+#greetings = ['Hello', 'Hi', 'Hey', 'Howdy' ,'Hola']
+
+#value = random.choice(greetings)
+#print(value)
+
+#import random
+
+#colors = ['Red' , 'Black' , 'Green']
+
+#results = random.choices(colors, k=10)
+#print(value +'_corey_')
+#print(results)
+
+#import random
+
+#deck = list(range(1, 53))
+
+#random.shuffle(deck)
+#print(deck)
+
+#import random
+
+#deck = list(range())
+
+import random
+first_names = ['John', 'Jane', 'Corey', 'Travis', 'Dave', 'Kurt', 'Neil', 'Sam', 'Steve', 'Tom', 'James', 'Robert', 'Michael', 'Charles', 'Joe', 'Mary', 'Maggie', 'Nicole', 'Patricia', 'Linda', 'Barbara', 'Elizabeth', 'Laura', 'Jennifer', 'Maria']
+
+last_names = ['Smith', 'Doe', 'Jenkins', 'Robinson', 'Davis', 'Stuart', 'Jefferson', 'Jacobs', 'Wright', 'Patterson', 'Wilks', 'Arnold', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor', 'Anderson', 'Thomas', 'Jackson', 'White', 'Harris', 'Martin']
+
+street_names = ['Main', 'High', 'Pearl', 'Maple', 'Park', 'Oak', 'Pine', 'Cedar', 'Elm', 'Washington', 'Lake', 'Hill']
+
+fake_cities = ['Metropolis', 'Eerie', "King's Landing", 'Sunnydale', 'Bedrock', 'South Park', 'Atlantis', 'Mordor', 'Olympus', 'Dawnstar', 'Balmora', 'Gotham', 'Springfield', 'Quahog', 'Smalltown', 'Epicburg', 'Pythonville', 'Faketown', 'Westworld', 'Thundera', 'Vice City', 'Blackwater', 'Oldtown', 'Valyria', 'Winterfell', 'Braavos‎', 'Lakeview']
+
+states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+
+for num in range(100):
+    first = random.choice(first_names)
+   last = random.choice(last_names)
+
+   street_num = random.randint(100, 999)
+    street = random.choice(street_names)
+    city = random.choice(fake_cities)
+    state = random.choice(states)
+    zip_code = random.randint(10000, 99999)
+    address = f'{street_num} {street} St., {city} {state} {zip_code}'
+
+    email = first.lower() + last.lower() + '@bogusemail.com'
+
+    print(f'{first} {last}\n{phone}\n{address}\n{email}\n')
+
